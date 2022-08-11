b059fcca4eddc1690707eae35ca3e94afbcb45df
AnujVaghani <anujvaghani0@gmail.com>
2022-08-11 11:56:28 +0530

    leetcode questions

---
+++ src/Array/SetMatrixZeroes.java
@@ -0,0 +1,52 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SetMatrixZeroes {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+//     int[][] ans=new int[arr.length][arr.length];
+//        System.out.println(Arrays.toString(ans));
+        setZeroes(arr);
+    }
+
+
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    for (int k = 0; k < m; k++) {
+                        if (matrix[i][k] != 0) {
+                            matrix[i][k] = -9999;
+                        }
+                    }
+                }
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                if (matrix[i][j] == 0) {
+                    for (int k = 0; k < n; k++) {
+                        if (matrix[k][j] != 0) {
+                            matrix[k][j] = -9999;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == -9999) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ src/Array/palindrome.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class palindrome {
+    public static void main(String[] args) {
+        int n = 454;
+        System.out.println(palindrome(n));
+    }
+
+    public static boolean palindrome(int n) {
+        int ans = 0;
+        int temp = n;
+        while (n > 0) {
+            int r = n % 10;
+            ans = (ans * 10) + r;
+            n = n / 10;
+        }
+        if (ans == temp) {
+            return true;
+        }
+        return false;
+    }
+}
