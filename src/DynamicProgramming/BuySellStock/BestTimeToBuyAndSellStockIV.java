d08ab4639f474ba046babd7d8d33e2e5b9ac6892
AnujVaghani <anujvaghani0@gmail.com>
2022-12-04 22:57:01 +0530

    add

--- src/DynamicProgramming/BuySellStock/BestTimeToBuyAndSellStockIV.java
+++ src/DynamicProgramming/BuySellStock/BestTimeToBuyAndSellStockIV.java
@@ -2,8 +2,41 @@ package DynamicProgramming.BuySellStock;
 
 public class BestTimeToBuyAndSellStockIV {
     public static void main(String[] args) {
-
+    int[] prices={2,4,1};
+        System.out.println(maxProfit(2,prices));
     }
 
+    public static int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2 * k];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 0; j < 2 * k; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return solve(prices, 0, 0, k, dp);
+    }
 
+    private static int solve(int[] prices, int index, int operationNumber, int k, int[][] dp) {
+        if (index == prices.length) {
+            return 0;
+        }
+        if (operationNumber == 2 * k) {
+            return 0;
+        }
+        if (dp[index][operationNumber] != -1) {
+            return dp[index][operationNumber];
+        }
+        int profit = 0;
+        if (operationNumber % 2 == 0) {
+            int takeBuy = -prices[index] + solve(prices, index + 1, operationNumber + 1, k, dp);
+            int skipBuy = solve(prices, index + 1, operationNumber, k, dp);
+            profit = Math.max(takeBuy, skipBuy);
+        } else {
+            int takeSell = prices[index] + solve(prices, index + 1, operationNumber + 1, k, dp);
+            int skipSell = solve(prices, index + 1, operationNumber, k, dp);
+            profit = Math.max(takeSell, skipSell);
+        }
+        return dp[index][operationNumber] = profit;
+    }
 }
--- src/DynamicProgramming/BuySellStock/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/DynamicProgramming/BuySellStock/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,9 +1,41 @@
 package DynamicProgramming.BuySellStock;
 
+// Question link -> https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 public class BestTimeToBuyAndSellStockWithTransactionFee {
     public static void main(String[] args) {
-
+    int[] prices={1,3,2,8,4,9};
+        System.out.println(maxProfit(prices,2));
     }
 
+    public static int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 0; j < 2; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return solve(prices, 0, 1, fee, dp);
+    }
 
+    private static int solve(int[] prices, int index, int Buy, int fee, int[][] dp) {
+        if (index == prices.length) {
+            return 0;
+        }
+        if (dp[index][Buy] != -1) {
+            return dp[index][Buy];
+        }
+
+        int profit = 0;
+        if (Buy == 1) {
+            int take = -prices[index] + solve(prices, index + 1, 0, fee, dp);
+            int SkipBuy = solve(prices, index + 1, 1, fee, dp);
+            profit = Math.max(take, SkipBuy);
+        } else {
+            int takeSell = prices[index] + solve(prices, index + 1, 1, fee, dp)-fee;
+            int SkipSell = solve(prices, index + 1, 0, fee, dp);
+            profit = Math.max(takeSell, SkipSell);
+        }
+        return dp[index][Buy] = profit;
+
+    }
 }
